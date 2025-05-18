e4c73803656efa4439a15720016841f38656baa5
Jonas Winkler <j.winkler@ifw-dresden.de>
2025-05-18 11:18:13 +0200

    Formatting

--- src/magmeas/magmeas.py
+++ src/magmeas/magmeas.py
@@ -3,10 +3,10 @@
 import json
 from pathlib import Path
 
+import h5py
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import h5py
 from matplotlib.ticker import MultipleLocator
 
 mu_0 = np.pi * 4e-7
@@ -129,8 +129,7 @@ class VSM:
             + (a2 * a + b2 * b - 2 * c2 * c) / (3 * abc)
             + ((a2 + b2 - 2 * c2) / (3 * abc)) * r_abc
             + (c / ab) * (r_ac + r_bc)
-            - (r_ab * r_ab * r_ab + r_bc * r_bc *
-               r_bc + r_ac * r_ac * r_ac) / (3 * abc)
+            - (r_ab * r_ab * r_ab + r_bc * r_bc * r_bc + r_ac * r_ac * r_ac) / (3 * abc)
         )
         # divide out the factor of pi
         D = pi_Dz / np.pi
@@ -261,8 +260,7 @@ class VSM:
         # let's only look at M(T) during cooling, this is usually more reliable
         # cooling is assumed to occur after half of the measurement time
         # also cut off last couple of measurement points as they are unstable
-        selec = (self.t > np.max(self.t) * 0.5) * \
-            (self.t < np.max(self.t) * 0.9)
+        selec = (self.t > np.max(self.t) * 0.5) * (self.t < np.max(self.t) * 0.9)
         nM = nM[selec]
         T = self.T[selec]
         # generous kernel for smoothing
@@ -290,13 +288,12 @@ class VSM:
         None
 
         """
-
         self.path = Path(datfile)
 
         def rextract(string, startsub, endsub):
             endind = string.index(endsub)
             startind = string.rindex(startsub, 0, endind)
-            return string[startind + len(startsub): endind]
+            return string[startind + len(startsub) : endind]
 
         err = """
               Sample parameters could not be read automatically.
@@ -313,8 +310,7 @@ class VSM:
                 s = str(f.read(-1))
 
             try:
-                mass = float(rextract(s, "INFO,", ",SAMPLE_MASS")
-                             ) * 1e-3  # mass in g
+                mass = float(rextract(s, "INFO,", ",SAMPLE_MASS")) * 1e-3  # mass in g
             except ValueError:
                 raise Exception(err) from None
 
@@ -332,11 +328,9 @@ class VSM:
             print("Manual input method selected")
             mass = float(input("Sample mass in mg: "))
             print(f"mass = {mass} mg")
-            a = float(
-                input("Sample dimension a (perpendicular to field) in mm: "))
+            a = float(input("Sample dimension a (perpendicular to field) in mm: "))
             print(f"a = {a} mm")
-            b = float(
-                input("Sample dimension b (perpendicular to field) in mm: "))
+            b = float(input("Sample dimension b (perpendicular to field) in mm: "))
             print(f"b = {b} mm")
             c = float(input("Sample dimension c (parallel to field) in mm: "))
             print(f"c = {c} mm")
@@ -513,10 +507,8 @@ class VSM:
         ax1.yaxis.set_minor_locator(MultipleLocator(0.05))
         ax1.set_xlabel(r"$\mu_0 H_{int}$ in $T$")
         ax1.set_ylabel(r"$J$ in $T$")
-        ax1.grid(visible=True, which="major",
-                 axis="both", linestyle=":", linewidth=1)
-        ax1.grid(visible=True, which="minor", axis="both",
-                 linestyle=":", linewidth=0.5)
+        ax1.grid(visible=True, which="major", axis="both", linestyle=":", linewidth=1)
+        ax1.grid(visible=True, which="minor", axis="both", linestyle=":", linewidth=0.5)
         if label is not None:
             ax1.legend()
         fig.tight_layout()
@@ -575,8 +567,7 @@ class VSM:
         """
         fig, ax = plt.subplots(1, 1, figsize=(16 / 2.54, 12 / 2.54))
 
-        ax.plot(self.T[self.t > max(self.t) / 2],
-                self.M[self.t > max(self.t) / 2])
+        ax.plot(self.T[self.t > max(self.t) / 2], self.M[self.t > max(self.t) / 2])
 
         ax.set_xlabel("Temperature in K")
         ax.set_ylabel("Magnetic moment in a.u.")
@@ -657,7 +648,6 @@ class VSM:
         -------
         None
         """
-
         print(f"\n\n{self.path.name}:\n")
 
         if self.measurement == "M(H)":
@@ -672,7 +662,7 @@ class VSM:
         for key in properties:
             print(f"{key} = {properties[key][0]}")
 
-    def to_hdf5(self, unit='T'):
+    def to_hdf5(self, unit="T"):
         """
         Save .DAT-file and calculated properties in hdf5 file.
 
@@ -687,41 +677,37 @@ class VSM:
         """
         with open(self.path) as f:
             s = str(f.read(-1))
-        head = s[s.index('INFO'):s.rindex('\nDATATYPE')]
-        head = head.split('\n')
-        info = {i[i.rindex(',')+1:]: i[i.index(',')+1:i.rindex(',')]
-                for i in head}
+        head = s[s.index("INFO") : s.rindex("\nDATATYPE")]
+        head = head.split("\n")
+        info = {
+            i[i.rindex(",") + 1 :]: i[i.index(",") + 1 : i.rindex(",")] for i in head
+        }
         df = pd.read_csv(self.path, skiprows=34, encoding="cp1252")
 
-        f = h5py.File(self.path.parent.joinpath(self.path.stem + '.hdf5'), 'a')
-        for i in info.keys():
-            f.create_dataset('Info/'+i, data=info[i])
-        f['Info/SAMPLE_MASS'].attrs['unit'] = 'mg'
-        f['Info/SAMPLE_SIZE'].attrs['unit'] = 'mm'
+        f = h5py.File(self.path.parent.joinpath(self.path.stem + ".hdf5"), "a")
+        for i in info:
+            f.create_dataset("Info/" + i, data=info[i])
+        f["Info/SAMPLE_MASS"].attrs["unit"] = "mg"
+        f["Info/SAMPLE_SIZE"].attrs["unit"] = "mm"
 
         for i in df.columns:
             dat = np.array(df[i])
-            if dat.dtype != 'O':
-                f.create_dataset('Data/'+i, data=dat)
-            if dat.dtype == 'O':
-                f.create_dataset('Data/'+i, data=[str(j) for j in df[i]])
+            if dat.dtype != "O":
+                f.create_dataset("Data/" + i, data=dat)
+            if dat.dtype == "O":
+                f.create_dataset("Data/" + i, data=[str(j) for j in df[i]])
 
         if self.measurement == "M(H)":
-            f.create_dataset('Properties/Remanence',
-                             data=self.get_remanence(unit))
-            f['Properties/Remanence'].attrs['unit'] = unit
-            f.create_dataset('Properties/Coercivity',
-                             data=self.get_coercivity(unit))
-            f['Properties/Coercivity'].attrs['unit'] = unit
-            f.create_dataset('Properties/BHmax',
-                             data=self.get_remanence(unit))
-            f['Properties/BHmax'].attrs['unit'] = 'kJ/m^3'
-            f.create_dataset('Properties/Squareness',
-                             data=self.get_squareness())
+            f.create_dataset("Properties/Remanence", data=self.get_remanence(unit))
+            f["Properties/Remanence"].attrs["unit"] = unit
+            f.create_dataset("Properties/Coercivity", data=self.get_coercivity(unit))
+            f["Properties/Coercivity"].attrs["unit"] = unit
+            f.create_dataset("Properties/BHmax", data=self.get_remanence(unit))
+            f["Properties/BHmax"].attrs["unit"] = "kJ/m^3"
+            f.create_dataset("Properties/Squareness", data=self.get_squareness())
         elif self.measurement == "M(T)":
-            f.create_dataset('Properties/Tc',
-                             data=self.get_remanence(unit))
-            f['Properties/Tc'].attrs['unit'] = 'K'
+            f.create_dataset("Properties/Tc", data=self.get_remanence(unit))
+            f["Properties/Tc"].attrs["unit"] = "K"
 
 
 def plot_multiple_VSM(data, labels, filepath=None, demag=True):
@@ -760,10 +746,8 @@ def plot_multiple_VSM(data, labels, filepath=None, demag=True):
     ax1.yaxis.set_minor_locator(MultipleLocator(0.05))
     ax1.set_xlabel(r"$\mu_0 H_{int}$ in $T$")
     ax1.set_ylabel(r"$J$ in $T$")
-    ax1.grid(visible=True, which="major",
-             axis="both", linestyle=":", linewidth=1)
-    ax1.grid(visible=True, which="minor", axis="both",
-             linestyle=":", linewidth=0.5)
+    ax1.grid(visible=True, which="major", axis="both", linestyle=":", linewidth=1)
+    ax1.grid(visible=True, which="minor", axis="both", linestyle=":", linewidth=0.5)
     ax1.legend()
     fig.tight_layout()
     plt.gca().set_prop_cycle(None)
@@ -799,10 +783,8 @@ def plot_multiple_VSM(data, labels, filepath=None, demag=True):
         ax2.yaxis.set_minor_locator(MultipleLocator(0.05))
         ax2.set_xlabel(r"$\mu_0 H_{int}$ in $T$")
         ax2.set_ylabel(r"$J$ in $T$")
-        ax2.grid(visible=True, which="major",
-                 axis="both", linestyle=":", linewidth=1)
-        ax2.grid(visible=True, which="minor", axis="both",
-                 linestyle=":", linewidth=0.5)
+        ax2.grid(visible=True, which="major", axis="both", linestyle=":", linewidth=1)
+        ax2.grid(visible=True, which="minor", axis="both", linestyle=":", linewidth=0.5)
 
     # save figure if filepath is given
     if filepath is not None:
