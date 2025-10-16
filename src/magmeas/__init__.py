34ddfb291a2543bad299116a3868dba00f6a5a80
Jonas Winkler <j.winkler@ifw-dresden.de>
2025-10-16 11:22:31 +0200

    Fixed import of mult_properties_to_file()

--- src/magmeas/__init__.py
+++ src/magmeas/__init__.py
@@ -7,6 +7,6 @@ Automatic plotting with matplotlib is also available.
 
 __version__ = "0.4.2"
 
-from magmeas.magmeas import VSM, plot_multiple_VSM
+from magmeas.magmeas import VSM, mult_properties_to_file, plot_multiple_VSM
 
-__all__ = [VSM, plot_multiple_VSM]
+__all__ = [VSM, plot_multiple_VSM, mult_properties_to_file]
