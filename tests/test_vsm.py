825afca7735993eb4ae3c46acb0dae14e352db3c
Jonas Winkler <j.winkler@ifw-dresden.de>
2025-11-27 17:54:26 +0100

    Greatly extended unittest to now test export functionality

--- tests/test_vsm.py
+++ tests/test_vsm.py
@@ -1,8 +1,13 @@
 """Testing routine for magmeas.VSM objects."""
 
-import os
+from pathlib import Path
+
+import matplotlib
 
 import magmeas
+
+matplotlib.use("Agg")
+cwd = Path(__file__).parent.resolve()
 
 
 def test_version():
@@ -12,13 +17,117 @@ def test_version():
 
 def test_init_VSM_MH():
     """Test initialisation of M(H) measurement as magmeas.VSM object."""
-    example_file_path = os.path.join(os.path.dirname(__file__), "VSM_MH.DAT")
-    mh = magmeas.VSM(example_file_path)
+    file_path = cwd.joinpath("VSM_MH.DAT")
+    mh = magmeas.VSM(file_path)
     assert isinstance(mh, magmeas.VSM)
 
 
 def test_init_VSM_MT():
     """Test initialisation of M(T) measurement as magmeas.VSM object."""
-    example_file_path = os.path.join(os.path.dirname(__file__), "VSM_MT.DAT")
-    mh = magmeas.VSM(example_file_path)
-    assert isinstance(mh, magmeas.VSM)
+    file_path = cwd.joinpath("VSM_MT.DAT")
+    mt = magmeas.VSM(file_path)
+    assert isinstance(mt, magmeas.VSM)
+
+
+def test_VSM_MH_to_yml():
+    """Test export of M(H) measurement to YAML."""
+    file_path = cwd.joinpath("VSM_MH.DAT")
+    mh = magmeas.VSM(file_path)
+    output_path = cwd.joinpath(file_path.stem + "_properties.yml")
+    try:
+        mh.properties_to_file(output_path)
+    except Exception as err:
+        print(f"Unexpected {err=}, {type(err)=}")
+    assert output_path.is_file()
+    output_path.unlink()
+
+
+def test_VSM_MH_to_csv():
+    """Test export of M(H) measurement to CSV."""
+    file_path = cwd.joinpath("VSM_MH.DAT")
+    mh = magmeas.VSM(file_path)
+    output_path = cwd.joinpath(file_path.stem + "_properties.csv")
+    try:
+        mh.properties_to_file(output_path)
+    except Exception as err:
+        print(f"Unexpected {err=}, {type(err)=}")
+    assert output_path.is_file()
+    output_path.unlink()
+
+
+def test_VSM_MH_to_hdf5():
+    """Test export of M(H) measurement to HDF5."""
+    file_path = cwd.joinpath("VSM_MH.DAT")
+    mh = magmeas.VSM(file_path)
+    output_path = cwd.joinpath(file_path.stem + ".hdf5")
+    try:
+        mh.to_hdf5()
+    except Exception as err:
+        print(f"Unexpected {err=}, {type(err)=}")
+    assert output_path.is_file()
+    output_path.unlink()
+
+
+def test_VSM_MH_plot():
+    """Test export of M(H) measurement to PNG."""
+    file_path = cwd.joinpath("VSM_MH.DAT")
+    mh = magmeas.VSM(file_path)
+    output_path = cwd.joinpath(file_path.stem + ".png")
+    try:
+        mh.plot(output_path)
+    except Exception as err:
+        print(f"Unexpected {err=}, {type(err)=}")
+    assert output_path.is_file()
+    output_path.unlink()
+
+
+def test_VSM_MT_to_yml():
+    """Test export of M(T) measurement to YAML."""
+    file_path = cwd.joinpath("VSM_MT.DAT")
+    mh = magmeas.VSM(file_path)
+    output_path = cwd.joinpath(file_path.stem + "_properties.yml")
+    try:
+        mh.properties_to_file(output_path)
+    except Exception as err:
+        print(f"Unexpected {err=}, {type(err)=}")
+    assert output_path.is_file()
+    output_path.unlink()
+
+
+def test_VSM_MT_to_csv():
+    """Test export of M(T) measurement to CSV."""
+    file_path = cwd.joinpath("VSM_MT.DAT")
+    mh = magmeas.VSM(file_path)
+    output_path = cwd.joinpath(file_path.stem + "_properties.csv")
+    try:
+        mh.properties_to_file(output_path)
+    except Exception as err:
+        print(f"Unexpected {err=}, {type(err)=}")
+    assert output_path.is_file()
+    output_path.unlink()
+
+
+def test_VSM_MT_to_hdf5():
+    """Test export of M(T) measurement to HDF5."""
+    file_path = cwd.joinpath("VSM_MT.DAT")
+    mh = magmeas.VSM(file_path)
+    output_path = cwd.joinpath(file_path.stem + ".hdf5")
+    try:
+        mh.to_hdf5()
+    except Exception as err:
+        print(f"Unexpected {err=}, {type(err)=}")
+    assert output_path.is_file()
+    output_path.unlink()
+
+
+def test_VSM_MT_plot():
+    """Test export of M(T) measurement to PNG."""
+    file_path = cwd.joinpath("VSM_MT.DAT")
+    mh = magmeas.VSM(file_path)
+    output_path = cwd.joinpath(file_path.stem + ".png")
+    try:
+        mh.plot(output_path)
+    except Exception as err:
+        print(f"Unexpected {err=}, {type(err)=}")
+    assert output_path.is_file()
+    output_path.unlink()
